a9a2cccaa1f6e7faac82733c335c4326d70a0875
VISHNU C R <vishnucr3000@gmail.com>
2022-06-21 11:26:16 +0530

    kwargs

--- dictionary_works/accounts.py
+++ dictionary_works/accounts.py
@@ -37,31 +37,60 @@ accounts = [
 
 ]
 
-
 # print details of account 1002
 
-acdetails=[ac for ac in accounts if ac["acno"]==1002]
+acdetails = [ac for ac in accounts if ac["acno"] == 1002]
 
 print(acdetails)
 
 # print accounts of saving account type
 
-saving_acc=[ac["acno"] for ac in accounts if ac["ac_type"]=="savings"]
+saving_acc = [ac["acno"] for ac in accounts if ac["ac_type"] == "savings"]
 
 print(saving_acc)
 
 # sort account based on balance order by desc
 
-accounts.sort(reverse=True,key=lambda ac:ac["balance"])
+accounts.sort(reverse=True, key=lambda ac: ac["balance"])
 print(accounts)
 
-sort_acc=sorted(accounts,reverse=True,key=lambda ac:ac["balance"])
+sort_acc = sorted(accounts, reverse=True, key=lambda ac: ac["balance"])
 
 print(sort_acc)
 
 # print all transaction where transaction amount greater than 500
 
-acc_tran=[for ac in accounts if ac["transactions"]]
+for acc in accounts:
+    trans = [tr for tr in acc["transactions"] if tr["amount"] > 500]
+    print(trans)
 
-# print all phone pay transaction
+# print all phone pay transactions
+
+all_tran=[ac["transactions"] for ac in accounts]
+p_tran=[trans for tlist in all_tran for trans in tlist if trans["method"]=="phone-pay"]
+print(p_tran)
+
+# print all credit transactions of 1002
+
+amt=[amount for tlist in all_tran for amount in tlist if amount["to"]==1002]
+
+print(amt)
+
+# aggregate transactions based on payment mode
+agg={}
+ag_tran=[tran for at in all_tran for tran in at]
+for mtd in ag_tran:
+    p_method=mtd["method"]
+    p_amt=mtd["amount"]
+    if p_method in agg:
+        agg[p_method]+=p_amt
+    else:
+        agg[p_method]=p_amt
+max_tran=max(agg.items(),key=lambda item:item[1])
+print(max_tran)
+
+
+
+
+
 
---
+++ kwargs/kwargs.py
@@ -0,0 +1,16 @@
+
+
+def add(*args):
+    return sum(args)
+print(add(10,20,30))
+
+
+def max_func(*mx):
+    return max(mx)
+print(max_func(10,20,30))
+
+
+def min_fun(*mn):
+    return min(mn)
+print(min_fun(10,20,30))
+
